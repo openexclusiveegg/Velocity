57e51becb3fcecedf299de712582dc54937cf4cc
Matthew Mace <105390001+openexclusiveegg@users.noreply.github.com>
2023-07-02 19:39:27 -0700

    Update newTab.tsx

--- src/routes/internal/newTab.tsx
+++ src/routes/internal/newTab.tsx
@@ -28,7 +28,7 @@ export default function NewTab(): JSX.Element {
       {/*  */}
       <div class="m-5 mt-32 flex items-center gap-5">
         <div class="h-20 w-20" id="logo"></div>
-        <h1 class="text-4xl font-semibold">Velocity</h1>
+        <h1 class="text-4xl font-semibold">Proxnow</h1>
       </div>
       <input
         class="m-5 rounded-md px-5 py-4 text-sm shadow-lg focus:shadow-2xl focus:outline-none focus:ring-0 md:w-1/2"
